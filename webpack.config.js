9adcf7cbcb642021c3ea04eb84cee41b8baf4212
Megan McKissack <meganmckissack@gmail.com>
2022-06-03 16:34:52 -0700

    update template files

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(), 
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Project',
       template: './src/index.html',
       inject: 'body',
     }),
